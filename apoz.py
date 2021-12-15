5257a80046340cb600070a8fb37eea02527e2d47
Dipti S <diptibits2010@gmail.com>
2021-12-15 22:25:57 +0530

    Removing harcoding
    changing apoz
    changing the trim

--- apoz.py
+++ apoz.py
@@ -1,29 +1,47 @@
 import numpy as np
 import torch.nn as nn
-from vgg import feature_cfgs, classifier_cfgs
 from helper import valid
 
 
 class APoZ:
     def __init__(self, model):
-        self.model = model
+        self.set_model_and_cfgs(model)
         self.idx = 0
+        self.hook_handles = []
+        print(f"Layer(ReLU + Linear) {self.num_layer} module register")
+
+    # get the # filters in features and classifiers.
+    def set_model_and_cfgs(self, model):
         self.num_layer = 0
         self.apoz = []
-        self.hook_handles = []
+        self.model = model
+        cfg = []
+        cfg_names = []
+        for name, layer in self.model.named_modules():
+            if isinstance(layer, (nn.Conv2d)):
+                cfg.append(layer.out_channels)
+                cfg_names.append(f"{name}_conv")
+
+            if isinstance(layer, (nn.Linear)):
+                cfg.append(layer.out_features)
+                cfg_names.append(f"{name}_linear")
+
+            if isinstance(layer, (nn.AdaptiveAvgPool2d, nn.MaxPool2d)):
+                cfg.append('M')
+
+        # delete the last one since we don't trim it.
+        del cfg[-1]
+        del cfg_names[-1]
+        self.cfgs = cfg
+        self.layer_names = cfg_names
         # M is for Pooling layers.
-        for c in feature_cfgs + classifier_cfgs:
+        for c in self.cfgs:
             if c == 'M':
                 continue
-
             self.apoz.append([0] * c)
             self.num_layer += 1
 
         self.apoz = np.array(self.apoz, dtype=object)
-
-        self.register()
-
-        print(f"Layer(ReLU + Linear) {self.num_layer} module register")
 
     def get_zero_percent_hook(self, module, input, output):
         if output.dim() == 4:
@@ -43,13 +61,18 @@ class APoZ:
 
     # APoZ run after the output from the ReLU. ie in a forward hook
     def register(self):
-        for module in self.model.features.modules():
-            if type(module) == nn.ReLU:
-                self.hook_handles.append(module.register_forward_hook(self.get_zero_percent_hook))
+        # register function as forward hook for relu layer
+        for name, layer in self.model.named_modules():
+            if isinstance(layer, (nn.ReLU)):
+                self.hook_handles.append(layer.register_forward_hook(self.get_zero_percent_hook))
 
-        for module in self.model.classifier.modules():
-            if type(module) == nn.ReLU:
-                self.hook_handles.append(module.register_forward_hook(self.get_zero_percent_hook))
+        # for module in self.model.features.modules():
+        #     if type(module) == nn.ReLU:
+        #         self.hook_handles.append(module.register_forward_hook(self.get_zero_percent_hook))
+        #
+        # for module in self.model.classifier.modules():
+        #     if type(module) == nn.ReLU:
+        #         self.hook_handles.append(module.register_forward_hook(self.get_zero_percent_hook))
 
     def get_apoz(self, loader, criterion, device='cpu'):
         top1, top5 = valid(self.model,
--- trim_vgg_cifar10.py
+++ trim_vgg_cifar10.py
@@ -1,4 +1,3 @@
-import os
 import time
 import torch
 import argparse
@@ -11,13 +10,35 @@ from converter import conv_post_mask, linear_mask, linear_pre_mask
 from torchsummary import summary
 
 
-def trim_network(args, valid_loader, criterion, select_rate, itr):
+def find_display_apoz(apoz_obj, valid_loader, criterion, args):
+    apoz_obj.register()
+    apoz = apoz_obj.get_apoz(valid_loader, criterion, args.device)
+    save_pkl(apoz, args.apoz_path)
+    apoz_obj.deregister()
+    # if not os.path.exists(args.apoz_path):
+    #     apoz_obj.register()
+    #     apoz = apoz_obj.get_apoz(valid_loader, criterion, args.device)
+    #     save_pkl(apoz, args.apoz_path)
+    #     apoz_obj.deregister()
     #
-    start = time.time()
+    # else:
+    #     apoz = load_pkl(args.apoz_path)
+
     module_name = ['Conv 1-1', 'Conv 1-2', 'Conv 2-1', 'Conv 2-2', 'Conv 3-1',
                    'Conv 3-2', 'Conv 3-3', 'Conv 4-1', 'Conv 4-2', 'Conv 4-3',
                    'Conv 5-1', 'Conv 5-2', 'Conv 5-3', 'FC 6', 'FC 7']
 
+    # info display current apoz
+    print("Average Percentage Of Zero Mean")
+    for n, p in zip(apoz_obj.layer_names, apoz):
+        print(f"{n} : {p.mean() * 100 : .2f}%")
+
+    return apoz
+
+
+def trim_network(args, valid_loader, criterion, select_rate, itr):
+    #
+    start = time.time()
     # replace this by our model
     checkpoint = torch.load(args.load_path)
     model = vgg16(pretrained=False, n_class=10).to(args.device)
@@ -26,20 +47,8 @@ def trim_network(args, valid_loader, criterion, select_rate, itr):
     # show_summary(model) # what is this one?
     summary(model, (3, 32, 32))
     # save apoz pkl or load a new one
-    if not os.path.exists(args.apoz_path):
-        apoz_obj =APoZ(model)
-        apoz = apoz_obj.get_apoz(valid_loader, criterion, args.device)
-        save_pkl(apoz, args.apoz_path)
-        apoz_obj.deregister()
-
-    else:
-        apoz = load_pkl(args.apoz_path)
-
-    # info display current apoz
-    print("Average Percentage Of Zero Mean")
-    for n, p in zip(module_name, apoz):
-        print(f"{n} : {p.mean() * 100 : .2f}%")
-
+    apoz_obj = APoZ(model)
+    apoz = find_display_apoz(apoz_obj, valid_loader, criterion, args)
     # Masking
     mask = []
     # mask here, ie see what neurons to prune
@@ -54,8 +63,11 @@ def trim_network(args, valid_loader, criterion, select_rate, itr):
     # FC 7 [input]
     model.classifier[3] = linear_pre_mask(model.classifier[3], mask[1], device=args.device)
 
+    # update the apoz model and cfgs
+    apoz_obj.set_model_and_cfgs(model)
     # display the pruned model
     summary(model, (3, 32, 32))
+    new_apoz = find_display_apoz(apoz_obj, valid_loader, criterion, args)
     # save the pruned model
     torch.save({'cfg': ['Conv 5-3', 'FC 6'],
                 'mask': mask,
@@ -99,7 +111,6 @@ if __name__ == '__main__':
 
     # train/valid dataset
     _, loader_valid, _ = load_cifar10_data(batch=128)
-
     criterion = nn.CrossEntropyLoss().to(device)
     for ctr, one_rate in enumerate(rate):
         trim_network(args, loader_valid, criterion, one_rate, ctr)
